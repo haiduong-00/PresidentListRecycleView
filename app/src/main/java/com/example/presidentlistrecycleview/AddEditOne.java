3de85084f5412093e2255565e59270e7cfa688bd
haiduong-00 <dinhhaiduong203@gmail.com>
2024-03-08 13:37:05 +0700

    second commit

--- app/src/main/java/com/example/presidentlistrecycleview/AddEditOne.java
+++ app/src/main/java/com/example/presidentlistrecycleview/AddEditOne.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -15,8 +16,10 @@ public class AddEditOne extends AppCompatActivity {
     Button btn_ok, btn_cancel;
     List<President> presidentList;
     EditText et_presDate, et_presName, et_presImageURL;
+    TextView tv_presId;
     MyApplication myApplication = (MyApplication) this.getApplication();
 
+    int id;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,17 +32,47 @@ public class AddEditOne extends AppCompatActivity {
         et_presDate = findViewById(R.id.et_dateElection);
         et_presName = findViewById(R.id.et_presidentName);
         et_presImageURL = findViewById(R.id.et_pictureURL);
+        tv_presId = findViewById(R.id.tv_presidentIdNumber);
+
+        Intent intent = getIntent();
+        id = intent.getIntExtra("id", -1);
+        President president = null;
+
+        if(id >= 0) {
+            // edit the president
+            for (President p: presidentList) {
+                if(p.getId() == id) {
+                    president = p;
+                }
+            }
+            et_presName.setText(president.getName());
+            et_presDate.setText(String.valueOf(president.getDateOfElection()));
+            et_presImageURL.setText(president.getImageURL());
+            tv_presId.setText(String.valueOf(id));
+        }
+        else {
+            // create the president
+        }
 
         btn_ok.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // create President object
-                int nextId = MyApplication.getNextId();
-                President newPresident = new President(nextId, et_presName.getText().toString(), Integer.parseInt(et_presDate.getText().toString()), et_presImageURL.getText().toString());
-                // add the object at the global list of presidents.
 
-                presidentList.add(newPresident);
-                myApplication.setNextId(nextId++);
+                if(id >= 0) {
+                    // update
+                    President updatedPresident = new President(id, et_presName.getText().toString(), Integer.parseInt(et_presDate.getText().toString()), et_presImageURL.getText().toString());
+                    presidentList.set(id, updatedPresident);
+                } else {
+                    // add new president
+                    // create President object
+                    int nextId = MyApplication.getNextId();
+                    President newPresident = new President(nextId, et_presName.getText().toString(), Integer.parseInt(et_presDate.getText().toString()), et_presImageURL.getText().toString());
+                    // add the object at the global list of presidents.
+
+                    presidentList.add(newPresident);
+                    myApplication.setNextId(nextId++);
+                }
+
                 // go back to activity
 
 
--- app/src/main/java/com/example/presidentlistrecycleview/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/presidentlistrecycleview/RecyclerViewAdapter.java
@@ -1,6 +1,7 @@
 package com.example.presidentlistrecycleview;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,6 +9,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
@@ -37,6 +39,15 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         holder.tv_presName.setText(presidentList.get(position).getName());
         holder.tv_presElectionDate.setText(String.valueOf(presidentList.get(position).getDateOfElection())); // integer cannot be set as set text: integer -> string
         Glide.with(this.context).load(presidentList.get(position).getImageURL()).into(holder.iv_presPic);
+        holder.parentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // send the control to the EditOneItem activity
+                Intent intent = new Intent(context, AddEditOne.class);
+                intent.putExtra("id", presidentList.get(position).getId());
+                context.startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -48,12 +59,14 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         ImageView iv_presPic;
         TextView tv_presName;
         TextView tv_presElectionDate;
+        ConstraintLayout parentLayout;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             iv_presPic = itemView.findViewById(R.id.iv_presidentPicture);
             tv_presName = itemView.findViewById(R.id.tv_presidentName);
             tv_presElectionDate = itemView.findViewById(R.id.tv_dateElection);
+            parentLayout = itemView.findViewById(R.id.oneLinePresidentLayout);
         }
     }
 }
